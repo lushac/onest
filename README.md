3813a9a98cbce41a9817d92002f9c0252700b7ed
Łukasz Pazdur <lpazdur07@gmail.com>
2020-03-05 21:01:16 +0100

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# One
+
+Free theme created by Splawr, which can be used by everyone. Made especially for start-up/digital agency/freelancers.
+
+### Preview
+
+##### [Online](https://splawr.com/files/preview/one)
+
+##### Local:
+
+After downloading a theme from Splawr you can run it by opening index.html in your favourite browser.
+
+#### Edit
+
+If you want to edit your theme which you downloaded directly from our website you should edit index.html file in your IDE (Visual Studio Code, Atom, Sublime etc.). To edit how the website look just edit stylesheet files which you can find in /css folder.
+
+### Installation
+
+Using SCSS in this project require [Node.js](https://nodejs.org/).
+
+Install needed dependencies using and run it by running following commands:
+
+```sh
+npm install
+npm run sass
+```
+
+After you edit and save SCSS file Node will automaticly render and save css file into /dist/css folder.
+
+### Questions/Problems/Bugs
+
+Feel free to contact us using contact form which is available here: https://splawr.com/contact
+
+### License
+
+MIT
---
+++ dist/js/main.js
@@ -0,0 +1,60 @@
+const navbarTransition = () => {
+  const navbar = document.querySelector("#main-nav");
+  if (window.scrollY < 100) {
+    navbar.style.boxShadow = "none";
+  } else if (window.scrollY >= 100) {
+    navbar.style.boxShadow = "0 2px 4px -1px rgba(0, 0, 0, 0.25)";
+  }
+};
+
+class TypeWriter {
+  constructor() {
+    this.textData = ["adipisicing."];
+    this.currentText = "";
+    this.word = "";
+    this.count = 0;
+    this.index = 0;
+    this.speed = 300;
+    this.isDeleting = false;
+    this.type();
+  }
+
+  type() {
+    if (this.count >= this.textData.length) {
+      this.count = 0;
+    }
+
+    this.currentText = this.textData[this.count];
+
+    if (this.isDeleting) {
+      this.word = this.currentText.slice(0, --this.index);
+    } else {
+      this.word = this.currentText.slice(0, ++this.index);
+    }
+
+    if (this.isDeleting) {
+      this.speed = 150;
+    }
+
+    if (!this.isDeleting && this.word === this.currentText) {
+      this.speed = 600;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.word === "") {
+      this.isDeleting = false;
+      this.count++;
+      this.speed = 300;
+    }
+
+    const typewriter = document.querySelector(".typewriter");
+    typewriter.innerHTML = this.word;
+
+    setTimeout(() => this.type(), this.speed);
+  }
+}
+
+const init = () => {
+  new TypeWriter();
+};
+
+window.onscroll = navbarTransition;
+document.addEventListener("DOMContentLoaded", init());
